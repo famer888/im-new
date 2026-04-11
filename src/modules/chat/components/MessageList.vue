307490291a714d29c7d8e90374a43d579448c8e1
diannao <Matt@gmail.com>
2026-04-11 17:32:40 +0800

    style: 修改传输助手 背景颜色

--- src/modules/chat/components/MessageList.vue
+++ src/modules/chat/components/MessageList.vue
@@ -104,7 +104,7 @@ function handleItemResize(key: string, height: number) {
   overflow-x: hidden;
   position: relative;
   contain: strict;
-  background: #fff;
+  background: #f6f6f6;
 }
 
 .loading-indicator {
